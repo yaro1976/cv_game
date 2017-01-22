61d6157e43957fe48dca6be711ba9f00ac50fe20
Yaro76 <thierry.aronoff@gmail.com>
2017-01-22 21:37:02 +0000

    change architecture of the app

--- gulpfile.js
+++ gulpfile.js
@@ -1,60 +1,82 @@
 'use strict';
 
-var gulp        = require('gulp'),
-    sass        = require('gulp-sass'),
-    cssmin      = require('gulp-cssmin'),
-    rename      = require('gulp-rename'),
-    prefix      = require('gulp-autoprefixer'),
-    uglify      = require('gulp-uglify'),
-    concat      = require('gulp-concat'),
-    imagemin    = require('gulp-imagemin'),
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    cssmin = require('gulp-cssmin'),
+    rename = require('gulp-rename'),
+    prefix = require('gulp-autoprefixer'),
+    uglify = require('gulp-uglify'),
+    concat = require('gulp-concat'),
+    imagemin = require('gulp-imagemin'),
     browserSync = require('browser-sync').create();
+var jslint = require('gulp-jslint-simple');
+
+
+// Jslint
+gulp.task('lint', function () {
+    gulp.src('public/src/js/**/*.js')
+        .pipe(jslint.run({
+            // project-wide JSLint options
+            node: true,
+            vars: true
+        }))
+        .pipe(jslint.report({
+            // example of using a JSHint reporter
+            reporter: require('jshint-stylish').reporter
+        }));
+});
+
 
 // Static Server + watching scss/html files
 gulp.task('serve', ['sass', 'js'], function() {
 
     browserSync.init({
-        server: './',
-        browser: "google chrome"
+        server: './public',
+        browser: "google chrome",
+        open: false
     });
 
-    gulp.watch('src/scss/**/*.scss', ['sass']);
-    gulp.watch('src/js/**/*.js', ['js']);
-    gulp.watch('./*.html').on('change', browserSync.reload);
+    gulp.watch('public/src/scss/**/*.scss', ['sass']);
+    gulp.watch('public/src/js/**/*.js', ['js']);
+    gulp.watch('./public/*.html').on('change', browserSync.reload);
 });
 
 // Configure CSS tasks.
-gulp.task('sass', function () {
-  return gulp.src('src/scss/**/*.scss')
-    .pipe(sass.sync().on('error', sass.logError))
-    .pipe(prefix('last 2 versions'))
-    .pipe(cssmin())
-    .pipe(rename({suffix: '.min'}))
-    .pipe(gulp.dest('dist/css'))
-    .pipe(browserSync.stream());
+gulp.task('sass', function() {
+    return gulp.src('public/src/scss/**/*.scss')
+        .pipe(sass.sync().on('error', sass.logError))
+        .pipe(prefix('last 2 versions'))
+        .pipe(cssmin())
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(gulp.dest('public/dist/css'))
+        .pipe(browserSync.stream());
 });
 
 // Configure JS.
 gulp.task('js', function() {
-  return gulp.src('src/js/**/*.js')
-    .pipe(uglify())
-    .pipe(concat('app.js'))
-    .pipe(rename({suffix: '.min'}))
-    .pipe(gulp.dest('dist/js'))
-    .pipe(browserSync.stream());
+    return gulp.src('public/src/js/**/*.js')
+        .pipe(uglify())
+        .pipe(concat('app.js'))
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(gulp.dest('public/dist/js'))
+        .pipe(browserSync.stream());
 });
 
 // Configure image stuff.
-gulp.task('images', function () {
-  return gulp.src('src/img/**/*.+(png|jpg|gif|svg)')
-    .pipe(imagemin())
-    .pipe(gulp.dest('dist/img'));
+gulp.task('images', function() {
+    return gulp.src('public/src/img/**/*.+(png|jpg|gif|svg)')
+        .pipe(imagemin())
+        .pipe(gulp.dest('public/dist/img'));
 });
 
-gulp.task('watch', function () {
-  gulp.watch('src/scss/**/*.scss', ['sass']);
-  gulp.watch('src/js/**/*.js', ['js']);
-  gulp.watch('./*.html').on('change', browserSync.reload);
+gulp.task('watch', function() {
+    gulp.watch('public/src/scss/**/*.scss', ['sass']);
+    gulp.watch('public/src/js/**/*.js', ['js']);
+    gulp.watch('./public/*.html').on('change', browserSync.reload);
 });
 
-gulp.task('default', ['sass', 'js', 'images', 'serve']);
+gulp.task('default', ['sass', 'lint','js', 'images', 'serve']);
--- server.js
+++ server.js
@@ -5,11 +5,11 @@ var express = require('express');
 var app = express();
 
 // Define port
-var port = process.env.PORT;
-var ip = process.env.IP;
+var port = process.env.PORT || "8080";
+var ip = process.env.IP || "0.0.0.0";
 
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res){
    console.log("req=",req," response=",res); 
