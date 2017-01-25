e2b8c437bbb40b56b4b75995e514133cd0156927
Yaro76 <thierry.aronoff@gmail.com>
2017-01-25 09:10:18 +0000

    Create the vertical colision check procedure

--- tests/gameboard.spec.js
+++ tests/gameboard.spec.js
@@ -1,4 +1,6 @@
+/* global Gameboard, chai */
 'use strict';
+
 var expect = chai.expect;
 
 describe('#Gameboard', function () {
@@ -66,6 +68,60 @@ describe('#Gameboard', function () {
                     gameboard.addElement('vaisseau', 10, 15, 50, 50);
                     expect(gameboard.moveY('vaisseau')).to.equal(14);
                 });
+                describe('Test if points are in the same zone', function () {
+                    describe('Vertical alignement', function () {
+                        describe('Test if the first element is outside the zone', function () {
+                            it('Should return be outside', function () {
+                                var gameboard = new Gameboard();
+                                expect(gameboard.testVerticalZone(10, 70, 50, 50)).to.equal(false);
+                            });
+                        });
+                        describe('Test if the second element is outside the zone', function () {
+                            it('Should return be outside', function () {
+                                var gameboard = new Gameboard();
+                                expect(gameboard.testVerticalZone(70, 10, 50, 50)).to.equal(false);
+                            });
+                        });
+                        describe('Test if the first element is inside the zone', function () {
+                            it('Should return be outside', function () {
+                                var gameboard = new Gameboard();
+                                expect(gameboard.testVerticalZone(10, 50, 50, 50)).to.equal(true);
+                            });
+                        });
+                        describe('Test if the second element is inside the zone', function () {
+                            it('Should return be outside', function () {
+                                var gameboard = new Gameboard();
+                                expect(gameboard.testVerticalZone(50, 10, 50, 50)).to.equal(true);
+                            });
+                        });
+                    });
+                    describe('horizontal alignement', function () {
+                        describe('Test if the first element is outside the zone', function () {
+                            it('Should return be outside', function () {
+                                var gameboard = new Gameboard();
+                                expect(gameboard.testHorizontalZone(10, 70, 50, 50)).to.equal(false);
+                            });
+                        });
+                        describe('Test if the second element is outside the zone', function () {
+                            it('Should return be outside', function () {
+                                var gameboard = new Gameboard();
+                                expect(gameboard.testHorizontalZone(70, 10, 50, 50)).to.equal(false);
+                            });
+                        });
+                        describe('Test if the first element is inside the zone', function () {
+                            it('Should return be outside', function () {
+                                var gameboard = new Gameboard();
+                                expect(gameboard.testHorizontalZone(10, 50, 50, 50)).to.equal(true);
+                            });
+                        });
+                        describe('Test if the second element is inside the zone', function () {
+                            it('Should return be outside', function () {
+                                var gameboard = new Gameboard();
+                                expect(gameboard.testHorizontalZone(50, 10, 50, 50)).to.equal(true);
+                            });
+                        });
+                    });
+                });
             });
 
             describe('Move not available', function () {
@@ -102,31 +158,56 @@ describe('#Gameboard', function () {
 
                 describe('Check of the element with other elements', function () {
                     describe('No elements arround', function () {
-                        it('should authorize the move');
-                    });
-                    describe('Elements present to left', function () {
-                        it('should refused the move');
-                    });
-                    describe('Elements present to right', function () {
-                        it('should refused the move');
-                    });
-                    describe('Elements present to top', function () {
-                        it('should refused the move');
-                    });
-                    describe('Elements present to bottom', function () {
-                        it('should refused the move');
-                    });
+                        describe('Move to bottom', function () {
+                            it('should authorize the move', function () {
+                                var gameboard = new Gameboard();
+                                gameboard.addElement('vaisseau', 10, 15, 50, 50);
+                                gameboard.addElement('vaisseau2', 80, 85, 50, 50);
+                                expect(gameboard.checkMoveX('vaisseau', "bottom")).to.equal(true);
+                            });
+                            describe('Move to top', function () {
+                                it('should authorize the move');
+                            });
+                            describe('Move to left', function () {
+                                it('should authorize the move');
+                            });
+                            describe('Move to right', function () {
+                                it('should authorize the move');
+                            });
+                        });
 
-
+                    });
+                    describe('Element present arround', function () {
+                        describe('Elements present to left', function () {
+                            it('should refused the move');
+                        });
+                        describe('Elements present to right', function () {
+                            it('should refused the move');
+                        });
+                        describe('Elements present to top', function () {
+                            it('should refused the move');
+                        });
+                        describe('Elements present to bottom', function () {
+                            describe('Element strict one upside the other', function () {
+                                it('should refused the move', function () {
+                                    var gameboard = new Gameboard();
+                                    gameboard.addElement('vaisseau', 10, 15, 50, 50);
+                                    gameboard.addElement('vaisseau2', 10, 64, 50, 50);
+                                    expect(gameboard.checkMoveX('vaisseau', "bottom")).to.equal(false);
+                                });
+                            });
+                            describe('Element not strictly one upside the other', function () {
+                                it('should refused the move', function () {
+                                    var gameboard = new Gameboard();
+                                    gameboard.addElement('vaisseau', 15, 15, 50, 50);
+                                    gameboard.addElement('vaisseau2', 10, 64, 50, 50);
+                                    expect(gameboard.checkMoveX('vaisseau', "bottom")).to.equal(false);
+                                });
+                            });
+                        });
+                    });
                 });
-
-
-
-
             });
-
-
         });
-
     });
 });
