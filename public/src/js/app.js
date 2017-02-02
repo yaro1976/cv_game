3064e6490c6e0b407f00d5570a5714c379ab7615
Thierry Aronoff <thierry.aronoff@gmail.com>
2017-02-02 12:55:29 +0100

    Header position and background color

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -12,96 +12,96 @@ var Gameboard,
 var start = null;
 
 window.addEventListener('load', function () {
-    var width = 600,
-        height = 800,
-        ratio,
-        party,
-        containerWidth,
-        containerHeight,
-        start;
 
-    ratio = width / height;
+/*
+* Call when game starts
+*/
+    var playGame = function () {
+        var width = 600,
+            height = 800,
+            ratio,
+            party,
+            containerWidth,
+            containerHeight,
+            start;
 
-    // Get the container initial size
-    var container = document.getElementsByClassName("container")[0];
-    containerWidth = container.offsetWidth;
-    containerHeight = container.offsetHeight;
+        ratio = width / height;
 
-    console.log(container);
+        // Get the container initial size
+        var container = document.getElementsByClassName("container")[0];
+        containerWidth = container.offsetWidth;
+        containerHeight = container.offsetHeight;
 
-    if (containerWidth > 600) {
-        width = 600;
-        height = width / ratio;
-    } else {
-        width = containerWidth;
-        height = width / ratio;
-    }
+        console.log(container);
 
-    if (containerHeight > 800) {
-        height = 800;
-        width = height * ratio;
-    } else {
-        height = containerHeight;
-        width = height * ratio;
-    }
-    console.log(containerWidth, containerHeight);
-    console.log(width, height);
+        if (containerWidth > 600) {
+            width = 600;
+            height = width / ratio;
+        } else {
+            width = containerWidth;
+            height = width / ratio;
+        }
 
+        if (containerHeight > 800) {
+            height = 800;
+            width = height * ratio;
+        } else {
+            height = containerHeight;
+            width = height * ratio;
+        }
+        console.log(containerWidth, containerHeight);
+        console.log(width, height);
 
-// height = 800;
-
-    // Initialize Game
-    party = new Game(width, height);
-    party.init(width / 2, height - 100);
+        // Initialize Game
+        party = new Game(width, height);
+        party.init(width / 2, height - 100);
     
 
-    // Play the party
-    // party.start();
+        // Play the party   
 
-    start = null;
+        start = null;
 
-    // Initialize Game
-    // party = new Game(width, height);
-    // party.init(width, height)
+        var play = function (timestamp) {
+            // Main step        
+            if (!start) {
+                start = timestamp;
+            }
+            // Check keyboard
+            party.checkDirection();
+            // Show Planets and generate it
+            party.generatePlanets();
+            // Show and generate Ennemies
+            party.generateEnemy();
+            // Move ennemies ship
+            party.enemyDirection();
+            // Draw all active elements
+            party.draw();
 
-    var play = function (timestamp) {
-        // Main step        
-        if (!start) {
-            start = timestamp;
-        }
-        // Check keyboard
-        party.checkDirection();
-        // Show Planets and generate it
-        party.generatePlanets();
-        // Show and generate Ennemies
-        party.generateEnemy();
-        // Move ennemies ship
-        party.enemyDirection();
-        // Draw all active elements
-        party.draw();
-
-        if (!party.gameWin && !party.lost) {
-            party.animation.main = window.requestAnimationFrame(play);
-        }
-        if (party.gameWin || party.lost) {
-            window.cancelAnimationFrame(party.animation.main);
-            party.ended();
-            console.log("app cycle", party.tabElement);
-            console.log(party.lost);
-
-            // Verify, if party is ended
+            if (!party.gameWin && !party.lost) {
+                party.animation.main = window.requestAnimationFrame(play);
+            }
             if (party.gameWin || party.lost) {
+                window.cancelAnimationFrame(party.animation.main);
                 party.ended();
-            }
+                console.log("app cycle", party.tabElement);
+                console.log(party.lost);
 
-            if (party.gameWin) {
-                // User win the game
+                // Verify, if party is ended
+                if (party.gameWin || party.lost) {
+                    party.ended();
+                }
+
+                if (party.gameWin) {
+                    // User win the game
+                }
+                if (party.lost) {
+                    // User lost the game
+                }
             }
-            if (party.lost) {
-                // User lost the game
-            }
-        }
+        };
+        party.animation.main = window.requestAnimationFrame(play);
     };
-
-    party.animation.main = window.requestAnimationFrame(play);
+    
+    playGame();
+   
 });
